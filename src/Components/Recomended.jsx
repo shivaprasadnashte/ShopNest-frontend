76a0782af50a9cb5eb39a009cc20a9f0ce42b96f
shivaprasadnashte <nashted.ug21.ee@nitp.ac.in>
2024-01-17 15:51:48 +0530

    minor fixes

--- src/Components/Recomended.jsx
+++ src/Components/Recomended.jsx
@@ -2,22 +2,22 @@ import React from 'react'
 import RecomendedCard from './RecomendedCard'
 function Recomended() {
   return (
-   <div className=' m-5'>
-    <p className=' font-bold my-2 text-xl' >Recommended items</p>
-    <div className='gap-5 md:grid-cols-4 grid-cols-2 lg:grid-cols-5 grid'>
-      <RecomendedCard />
-      <RecomendedCard />
-      <RecomendedCard />
-      <RecomendedCard />
-      <RecomendedCard />
-      <RecomendedCard />
-      <RecomendedCard />
-      <RecomendedCard />
-      <RecomendedCard />
-      <RecomendedCard />
-      
+    <div className=' m-5'>
+      <p className=' font-bold my-2 text-xl' >Recommended items</p>
+      <div className='gap-5 md:grid-cols-4 grid-cols-2 lg:grid-cols-5 grid'>
+        <RecomendedCard />
+        <RecomendedCard />
+        <RecomendedCard />
+        <RecomendedCard />
+        <RecomendedCard />
+        <RecomendedCard />
+        <RecomendedCard />
+        <RecomendedCard />
+        <RecomendedCard />
+        <RecomendedCard />
+
       </div>
-   </div>
+    </div>
   )
 }
 
