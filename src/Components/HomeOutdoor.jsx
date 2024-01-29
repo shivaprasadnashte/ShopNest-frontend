6e39650a492d67a8b3366d329b9c8f014dcc4369
shivaprasadnashte <nashted.ug21.ee@nitp.ac.in>
2024-01-29 13:30:39 +0530

    minor fixes

--- src/Components/HomeOutdoor.jsx
+++ src/Components/HomeOutdoor.jsx
@@ -9,7 +9,7 @@ function HomeOutdoor() {
         <>
             <div className=' hidden justify-center lg:flex  '>
                 <div className=" bg-[url('/images/home.jpeg')] bg-cover h-72 w-1/4 ">
-                    <div className=' :p-5 text-sm lg:text-2xl font-bold w-1/2'>
+                    <div className=' :p-5 text-sm lg:text-2xl fon t-bold w-1/2'>
                         <p>
                             Home and outdoor
                         </p>
